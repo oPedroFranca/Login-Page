8820f0bf832f52fe8daf478622278b423d4b6bad
Pedro Henrique França <phmartins.developer@gmail.com>
2023-02-03 15:12:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-self-assign */
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word</h1>
+    </div>
+  );
+}
+
+export default App;
